d090a51f76dc57f8801cd7b99e5eb42d4349865c
workcom0 <90753448+workcom0@users.noreply.github.com>
2022-05-06 22:59:32 +0900

    220506

---
+++ 3.숫자야구/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    entry: {
+        app: './main.js',
+    },
+    module: {
+        rules: [{
+            
+        }],
+    },
+    plugins: [],
+    output: {
+        filename: 'app.js',
+        path: './dist',
+    },
+};
